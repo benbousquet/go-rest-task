4ca8e7e145472e2376b0b160b3fae21476840d15
benbousquet <bbousque@asu.edu>
2020-07-11 21:22:27 -0700

    🚀Added data persistance for getting, saving and deleting tasks

--- task/task.go
+++ task/task.go
@@ -14,23 +14,45 @@ type Task struct {
 }
 
 func GetTasks(c *fiber.Ctx) {
-
 	var tasks []Task
 	database.DBConn.Find(&tasks)
 	c.JSON(tasks)
 }
 
 func GetTask(c *fiber.Ctx) {
-
-	c.Send("single task with id")
+	var task Task
+	id := c.Params("id")
+	database.DBConn.First(&task, id)
+	if task.Title == "" {
+		c.Status(500).Send("Cannot find a task with ID of ", id)
+		return
+	}
+	c.JSON(task)
 }
 
 func NewTask(c *fiber.Ctx) {
-
-	c.Send("add a task")
+	// create new task
+	task := new(Task)
+	// put post req body onto task struct
+	if err := c.BodyParser(task); err != nil {
+		c.Status(503).Send(err)
+		return
+	}
+	// put into db
+	database.DBConn.Create(&task)
+	c.JSON(task)
 }
 
 func DeleteTask(c *fiber.Ctx) {
-
-	c.Send("delete a task")
+	var task Task
+	// get param and check if its in db
+	id := c.Params("id")
+	database.DBConn.First(&task, id)
+	if task.Title == "" {
+		c.Status(500).Send("No task could be found with that ID")
+		return
+	}
+	// delete task
+	database.DBConn.Delete(task)
+	c.Send("Successfully Deleted Task with ID of ", id)
 }
